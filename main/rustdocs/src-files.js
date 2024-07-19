65061a5e09c8698ebb4d9839b956d6dd264d99b7
george-cosma <george-cosma@users.noreply.github.com>
2024-07-19 05:50:49 +0000

    deploy: d5d36e0d7879b5c60ef35d6f4f96bfc86ece25e8

--- main/rustdocs/src-files.js
+++ main/rustdocs/src-files.js
@@ -1,5 +1,5 @@
 var srcIndex = new Map(JSON.parse('[\
 ["log",["",[],["__private_api.rs","lib.rs","macros.rs"]]],\
-["wasm",["",[["core",[["reader",[["types",[],["export.rs","function_code_header.rs","global.rs","import.rs","memarg.rs","mod.rs","values.rs"]]],["mod.rs","section_header.rs"]]],["error.rs","indices.rs","mod.rs"]],["execution",[],["assert_validated.rs","hooks.rs","label.rs","locals.rs","mod.rs","store.rs","value.rs","value_stack.rs"]],["validation",[],["code.rs","mod.rs"]]],["lib.rs"]]]\
+["wasm",["",[["core",[["reader",[["types",[],["export.rs","function_code_header.rs","global.rs","import.rs","memarg.rs","mod.rs","opcode.rs","values.rs"]]],["mod.rs","section_header.rs"]]],["error.rs","indices.rs","mod.rs"]],["execution",[],["assert_validated.rs","hooks.rs","label.rs","locals.rs","mod.rs","store.rs","value.rs","value_stack.rs"]],["validation",[],["code.rs","mod.rs"]]],["lib.rs"]]]\
 ]'));
 createSrcSidebar();
